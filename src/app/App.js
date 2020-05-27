ad432a3a2e581bc4b85c5823d9d9df5fc189c600
Oleksii Bohdan <oleksiibohdan@gmail.com>
2020-05-27 19:05:47 +0300

    Ordered project structure

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import HomepageExample from "../pages/homepage-example/homepage-example.component";
+
+function App() {
+  return (
+      <HomepageExample/>
+  );
+}
+
+export default App;
---
+++ src/components/example/example.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './example.styles.scss';
+
+const Example = ({text}) => (
+    <div className='example'>{text}</div>
+);
+
+export default Example;
