533222cd66dc9c890c0c1c42271a5ef24ab42344
Stas0105 <sokolski@ukr.net>
2020-05-30 22:27:41 +0300

    Додав блоки в 5 секцію і зробив мінімальну анімацію

---
+++ src/components/secondbtn/secondbtn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './second.style.scss';
+
+class Ssexit extends React.Component {
+    render() {
+        return(
+            < div className="ssscbtn">
+                <button className="sssecondbtn">x</button>
+           </div>
+        )
+    }
+}
+export default Ssexit;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ import Business from "../../components/business/business.component";
 import Aboutus from "../../components/aboutus/aboutus.component";
 import NavBar from "../../components/navbar/navbar.component";
 import OurPartners from "../../components/our-partners/our-partners.component";
+import Decision from "../../components/decision/decision.component";
 
 const Homepage = () => (
     <>
@@ -15,6 +16,7 @@ const Homepage = () => (
         />
         <OurPartners partnersTitle='Наші партнери'/>
         <Aboutus />
+        <Decision />
     </div>
     </>
 );
